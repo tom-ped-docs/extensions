3bf80e0524a18d9a857d83162c2e3f6a3616a560
Tomasz <tom.ped.docs@gmail.com>
2022-04-04 18:22:07 +0200

    'message'

--- chrome_extension_aem/ts/background.ts
+++ chrome_extension_aem/ts/background.ts
@@ -3,37 +3,37 @@
  */
 
 const S_URL = {
-  p5_aem: "https://aem.samsung.com/",
-  p5_aem_eu: "https://aem-eu.samsung.com/",
-  p5_aem_eu_shop: "https://aem-eu-shop.samsung.com/",
-  p5_login: "aemapi/user/login_sso?s_user_id=",
-  p5_login_reset: "542b562d2e9g30fgcg6b6422101b6e2d95333a3f2e3e49",
-  p5_qa: "https://qaweb.samsung.com/",
-  p5_qa_shop: "https://qaweb-shop.samsung.com/",
+  p5_aem:             "https://aem.samsung.com/",
+  p5_aem_eu:          "https://aem-eu.samsung.com/",
+  p5_aem_eu_shop:     "https://aem-eu-shop.samsung.com/",
+  p5_login:           "aemapi/user/login_sso?s_user_id=",
+  p5_login_reset:     "542b562d2e9g30fgcg6b6422101b6e2d95333a3f2e3e49",
+  p5_qa:              "https://qaweb.samsung.com/",
+  p5_qa_shop:         "https://qaweb-shop.samsung.com/",
   p5_task_management: "aem/taskmanagement",
-  p5_workflows: "notifications.html",
-  p5_purging: "aem/pim/setting/cdnpurge/forwardCDNpurgReq",
-  p5_pim_b2c: "aem/pim/b2c/product/main/forwardProductFamilyList",
-  p5_pim_b2b: "aem/bim/b2b/product/main/forwardProductFamilyList",
+  p5_workflows:       "notifications.html",
+  p5_purging:         "aem/pim/setting/cdnpurge/forwardCDNpurgReq",
+  p5_pim_b2c:         "aem/pim/b2c/product/main/forwardProductFamilyList",
+  p5_pim_b2b:         "aem/bim/b2b/product/main/forwardProductFamilyList",
 
-  p6_aem_ap: "https://p6-ap-author.samsung.com/",
-  p6_aem_eu: "https://p6-eu-author.samsung.com/",
-  p6_aem_us: "https://p6-us-author.samsung.com/",
-  p6_login: "aemapi/user/login_sso?s_user_email=",
-  p6_login_reset: "pedzich.t@samsung.com",
-  p6_qa: "https://p6-qa.samsung.com/",
-  p6_workflows: "aem/inbox",
-  p6_purging: "aem/pim/global/setting/cdnpurging/page/purgerequest/info",
-  p6_pim_b2c: "aem/pim/b2c/product/detail/main/page/list",
-  p6_pim_b2b: "aem/bim/b2b/product/detail/main/page/list",
+  p6_aem_ap:          "https://p6-ap-author.samsung.com/",
+  p6_aem_eu:          "https://p6-eu-author.samsung.com/",
+  p6_aem_us:          "https://p6-us-author.samsung.com/",
+  p6_login:           "aemapi/user/login_sso?s_user_email=",
+  p6_login_reset:     "pedzich.t@samsung.com",
+  p6_qa:              "https://p6-qa.samsung.com/",
+  p6_workflows:       "aem/inbox",
+  p6_purging:         "aem/pim/global/setting/cdnpurging/page/purgerequest/info",
+  p6_pim_b2c:         "aem/pim/b2c/product/detail/main/page/list",
+  p6_pim_b2b:         "aem/bim/b2b/product/detail/main/page/list",
 
-  editor_s: "editor.html/content/samsung/",
-  editor_e: ".html",
-  editor_off_s: "content/samsung/",
-  editor_off_e: ".html?wcmmode=disabled",
-  live: "https://www.samsung.com/",
-  sites: "sites.html/content/samsung/",
-  assets: "assets.html/content/dam/samsung/"
+  editor_s:           "editor.html/content/samsung/",
+  editor_e:           ".html",
+  editor_off_s:       "content/samsung/",
+  editor_off_e:       ".html?wcmmode=disabled",
+  live:               "https://www.samsung.com/",
+  sites:              "sites.html/content/samsung/",
+  assets:             "assets.html/content/dam/samsung/"
 };
 
 /*
@@ -41,46 +41,46 @@ const S_URL = {
  */
 
 const P_URL = {
-  pre_prod_aem: "https://author.pp.iqos.com/",
-  pre_prod_qa: "https://www.pp.iqos.com/",
-  pre_prod_qa_login: "cgi-bin/authorize.cgi",
-  pre_prod_veev_qa: "https://www.pp.veev-vape.com/",
+  pre_prod_aem:           "https://author.pp.iqos.com/",
+  pre_prod_qa:            "https://www.pp.iqos.com/",
+  pre_prod_qa_login:      "cgi-bin/authorize.cgi",
+  pre_prod_veev_qa:       "https://www.pp.veev-vape.com/",
   pre_prod_veev_qa_login: "edgeauth",
-  pre_prod_club_qa: "https://club.pp.iqos.com/",
+  pre_prod_club_qa:       "https://club.pp.iqos.com/",
   pre_prod_club_qa_login: "edgeauth",
 
-  prod_aem: "https://author.iqos.com/",
-  prod_live: "https://www.iqos.com/",
-  prod_veev_live: "https://www.veev-vape.com/",
-  prod_club_live: "https://www.iqosclub.com/",
+  prod_aem:               "https://author.iqos.com/",
+  prod_live:              "https://www.iqos.com/",
+  prod_veev_live:         "https://www.veev-vape.com/",
+  prod_club_live:         "https://www.iqosclub.com/",
 
-  login: "aem/start.html",
-  editor_s: "editor.html/content/",
-  editor_e: ".html",
-  editor_off_s: "content/",
-  editor_off_e: ".html?wcmmode=disabled",
-  live: ".html?gr=false",
-  sites: "sites.html/content/",
-  assets: "assets.html/content/dam/iqos/local",
-  grfalse: "?gr=false"
+  login:                  "aem/start.html",
+  editor_s:               "editor.html/content/",
+  editor_e:               ".html",
+  editor_off_s:           "content/",
+  editor_off_e:           ".html?wcmmode=disabled",
+  live:                   ".html?gr=false",
+  sites:                  "sites.html/content/",
+  assets:                 "assets.html/content/dam/iqos/local",
+  grfalse:                "?gr=false"
 };
 
 /*
  * ------------------------- ... -------------------------
  */
 
-let s_p5_login = "542b562d2e9g30fgcg6b6422101b6e2d95333a3f2e3e49";
-let s_p6_login = "pedzich.t@samsung.com";
-let s_aem = "p5_aem_eu_shop";
-let s_url = "pl";
+let s_p5_login =  "542b562d2e9g30fgcg6b6422101b6e2d95333a3f2e3e49";
+let s_p6_login =  "pedzich.t@samsung.com";
+let s_aem =       "p5_aem_eu_shop";
+let s_url =       "pl";
 
-let p_aem = "pre_prod_aem";
-let p_content = "pmisite";
-let p_url = "de/en/home";
+let p_aem =       "pre_prod_aem";
+let p_content =   "pmisite";
+let p_url =       "de/en/home";
 
 // Visibility
-let vis_s_aem = true;
-let vis_p_aem = true;
+let vis_s_aem =     true;
+let vis_p_aem =     true;
 let vis_utilities = true;
 
 let theme = "auto";
--- chrome_extension_aem/ts/image_export.ts
+++ chrome_extension_aem/ts/image_export.ts
@@ -32,35 +32,35 @@ chrome.storage.local.get("theme", ({ theme }) => {
 const BODY = document.querySelector("body") as HTMLBodyElement;
 
 const DIV_0 = document.createElement("div") as HTMLDivElement;
-DIV_0.setAttribute("class", "position-fixed bottom-0 start-0 rounded m-2 p-2 | f-app-surface-base ms-motion-slideUpIn");
+DIV_0.setAttribute("class", "body__container");
 DIV_0.setAttribute("id", "div_0");
 
 const BUTTON = document.createElement("button") as HTMLButtonElement;
-BUTTON.setAttribute("class", "btn btn-sm position-absolute top-0 start-100 translate-middle | icon f-text");
+BUTTON.setAttribute("class", "button-close");
 BUTTON.setAttribute("id", "button_chrome_close");
 BUTTON.setAttribute("type", "button");
 DIV_0.appendChild(BUTTON);
 
 const DIV_1 = document.createElement("div") as HTMLDivElement;
-DIV_1.setAttribute("class", "rounded p-2 | f-app-surface-layer");
+DIV_1.setAttribute("class", "block1");
 DIV_0.appendChild(DIV_1);
 
 const DIV_2 = document.createElement("div") as HTMLDivElement;
-DIV_2.setAttribute("class", "row g-2");
+DIV_2.setAttribute("class", "block1__container1");
 DIV_1.appendChild(DIV_2);
 
 const DIV_3 = document.createElement("div") as HTMLDivElement;
-DIV_3.setAttribute("class", "col");
+DIV_3.setAttribute("class", "block1__container2");
 DIV_2.appendChild(DIV_3);
 
 // .input-group
 
 const DIV_4 = document.createElement("div") as HTMLDivElement;
-DIV_4.setAttribute("class", "input-group input-group-sm");
+DIV_4.setAttribute("class", "block1__input-group");
 DIV_3.appendChild(DIV_4);
 
 const SPAN = document.createElement("span") as HTMLSpanElement;
-SPAN.setAttribute("class", "input-group-text | f-body f-text | f-body f-text");
+SPAN.setAttribute("class", "block1__input-group-text");
 DIV_4.appendChild(SPAN);
 
 const SPAN_CTN = document.createTextNode("src");
@@ -69,7 +69,7 @@ SPAN.appendChild(SPAN_CTN);
 // select
 
 const SELECT = document.createElement("select") as HTMLSelectElement;
-SELECT.setAttribute("class", "form-select form-select-sm | f-body f-text");
+SELECT.setAttribute("class", "block1__select");
 DIV_4.appendChild(SELECT);
 
 const OPTION_0 = document.createElement("option") as HTMLOptionElement;
@@ -87,24 +87,24 @@ const OPTION_1_CTN = document.createTextNode("not contains");
 OPTION_1.appendChild(OPTION_1_CTN);
 
 const DIV_5 = document.createElement("div") as HTMLDivElement;
-DIV_5.setAttribute("class", "col");
+DIV_5.setAttribute("class", "block1__container3");
 DIV_2.appendChild(DIV_5);
 
 // input
 
 const INPUT = document.createElement("input") as HTMLInputElement;
-INPUT.setAttribute("class", "form-control form-control-sm | f-body f-text");
+INPUT.setAttribute("class", "block1__input");
 INPUT.setAttribute("type", "text");
 INPUT.setAttribute("placeholder", "/pl");
 DIV_5.appendChild(INPUT);
 
 const DIV_6 = document.createElement("div") as HTMLDivElement;
-DIV_6.setAttribute("class", "rounded mt-2 p-2 | f-app-surface-layer");
+DIV_6.setAttribute("class", "block2");
 DIV_6.setAttribute("id", "div_6");
 DIV_0.appendChild(DIV_6);
 
 const DIV_7 = document.createElement("div") as HTMLDivElement;
-DIV_7.setAttribute("class", "overflow-scroll");
+DIV_7.setAttribute("class", "block2__container1");
 DIV_7.setAttribute("id", "div_7");
 DIV_6.appendChild(DIV_7);
 
@@ -114,11 +114,11 @@ let i: number = 0;
 if (IMGS !== null) {
   for (let img of Array.from(IMGS)) {
     const DIV = document.createElement("div") as HTMLDivElement;
-    DIV.setAttribute("class", "d-flex flex-column mb-2");
+    DIV.setAttribute("class", "block2__container2");
     DIV_7.appendChild(DIV);
 
     const SPAN_0 = document.createElement("span") as HTMLSpanElement;
-    SPAN_0.setAttribute("class", "text-nowrap | f-body f-text cascadia_code");
+    SPAN_0.setAttribute("class", "block2__title");
     DIV.appendChild(SPAN_0);
 
     const SPAN_0_CTN = document.createTextNode(`img ${i}:`);
@@ -127,14 +127,14 @@ if (IMGS !== null) {
     i++;
 
     const SPAN_1 = document.createElement("span") as HTMLSpanElement;
-    SPAN_1.setAttribute("class", "text-nowrap | f-caption f-text cascadia_code_italic");
+    SPAN_1.setAttribute("class", "block2__subtitle-src");
     DIV.appendChild(SPAN_1);
 
     const SPAN_1_CTN = document.createTextNode(`src: ${img.src}`);
     SPAN_1.appendChild(SPAN_1_CTN);
 
     const SPAN_2 = document.createElement("span") as HTMLSpanElement;
-    SPAN_2.setAttribute("class", "text-muted text-nowrap | f-caption f-text cascadia_code_italic");
+    SPAN_2.setAttribute("class", "block2__subtitle-alt");
     DIV.appendChild(SPAN_2);
 
     const SPAN_2_CTN = document.createTextNode(`alt: ${img.alt}`);
